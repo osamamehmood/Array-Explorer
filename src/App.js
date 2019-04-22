55fd0095563a56a9ff7f38060a87c252c2b2906e
Osama <Osamamehmood786@gmail.com>
2019-04-22 23:13:25 +1000

    WIP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import OptionSelector from './components/OptionSelector';
+// import OptionSelector from './components/OptionSelector';
 
 class ArrayExplorer extends Component {
   render() {
     return (
       <div>
-        <h1>
-          JavaScript Array Explorer
-        </h1>
         <OptionSelector />
       </div>
     );
--- src/components/OptionSelector.js
+++ src/components/OptionSelector.js
@@ -1,50 +1,96 @@
 import React, { Component } from 'react';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import { Dropdown } from 'semantic-ui-react'
+import Select from 'react-select';
+
+const options = [
+  {
+    label: 'Add items to others arrays',
+    value: 'Add items to others arrays',
+  },
+  {
+    label: 'Remove items',
+    value: 'Remove items',
+  },
+  {
+    label: 'Find items',
+    value: 'Find items',
+  },
+  {
+    label: 'Walk over items',
+    value: 'Walk over items',
+  },
+  {
+    label: 'Return a string',
+    value: 'Return a string',
+  },
+  {
+    label: 'Order an array',
+    value: 'Order an array',
+  },
+  {
+    label: 'Something else',
+    value: 'Something else',
+  },
+]
+
+const optionForAddingItems = [
+  {
+    label: 'Add element(s) to an array',
+    value: 'Add element(s) to an array',
+  },
+  {
+    label: 'Elements to the end of an array',
+    value: 'Elements to the end of an array',
+  },
+  {
+    label: 'Elements to the front of an array',
+    value: 'Elements to the front of an array',
+  },
+  {
+    label: 'this array to other array(s) and/or value(s)',
+    value: 'Walk over items',
+  },
+]
 
 
 class OptionSelector extends Component {
   state = {
-    selectedOption : ""
+    selectedOption : '',
+    showOptionsForAddingItems: false
   }
 
-  handleChange = (e) => {
-    console.log(e.target.value);
+  handleChange = e => {
+    let selectedOption = e;
     this.setState({
-      selectedOption : e.target.value
-    });
+      selectedOption: selectedOption.value,
+    })
   }
 
-  render() {
+  render(){
     return (
       <div>
-        <div> I have an array, I would like to
-          <FormControl>
-            <Select
-              style={{width: '300px'}}
-              value={this.state.selectedOption}
-              onChange={ e =>
-                {this.setState ({
-                  selectedOption : e.target.value
-                })}
-              }
-            >
-              <MenuItem value={10}>Add items or other arrays</MenuItem>
-              <MenuItem value={20}>Remove Items(s)</MenuItem>
-              <MenuItem value={20}>Find Items(s)</MenuItem>
-              <MenuItem value={30}>Walk over items</MenuItem>
-              <MenuItem value={30}>Return a string</MenuItem>
-              <MenuItem value={30}>Order an array</MenuItem>
-              <MenuItem value={30}>Something else</MenuItem>
-          </Select>
-          </FormControl>
+        <h1> JavaScript Array Explorer </h1>
+        <p>I have an array, I would like to</p>
+        <div style={{width: '300px'}}>
+          <Select
+            options={options}
+            onChange={this.handleChange}
+          />
+          { this.state.selectedOption === 'Add items to others arrays' ?
+          (
+            <div>
+              <p> I need to add </p>
+              <Select
+                options={optionForAddingItems}
+                onChange={this.handleChange}
+              />
+            </div>
+          )
+            : ''
+          }
         </div>
-
       </div>
     );
   }
-}
+};
 
-export default OptionSelector
+export default OptionSelector;
